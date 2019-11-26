355ee8481b40b7239c546f726930486cee867afe
Abby Personal Acct Howe <howe.abigail@gmail.com>
2019-11-25 20:55:12 -0600

    added steps 1 - 2c

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function pageLoad() {
+    console.log('window loaded');
+
+    let take_off = document.getElementById("takeoff");
+    //console.log(take_off);
+    take_off.addEventListener("click", function (event) {
+        confirm("Are you sure?");
+        if (true) {
+            document.getElementById('flightStatus').innerText = "Shuttle in flight."
+            document.getElementById("shuttleBackground").style.backgroundColor = "Blue";
+        }
+    });
+};
+
+
+
+window.onload = pageLoad;
